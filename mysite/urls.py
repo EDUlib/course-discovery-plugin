83a540874024eea7ed02117610bc8c500863725e
Pierre Mailhot <pierremailhot@i135497-sse.fil.umontreal.ca>
2018-01-08 17:09:50 -0500

    Changing urls.py to use catalog instead of polls

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url('polls/', include('polls.urls')),
+    url('catalog/', include('catalog.urls')),
     url('admin/', admin.site.urls),
 ]
