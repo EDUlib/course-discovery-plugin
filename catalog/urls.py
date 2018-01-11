c2b102b30e0313a69b9e501eb07f5b4338048a6d
Pierre Mailhot <pierremailhot@i135497-sse.fil.umontreal.ca>
2018-01-11 10:51:31 -0500

    Quick fix to mysite/urls.py and catalog/urls.py after learning new trick

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url('(?P<school>[a-zA-Z0-9]+)', views.institution),
     url('', views.index, name='index'),
-    url(r'^$', views.index, name='index'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url('catalog', include('catalog.urls')),
-    url('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^catalog/', include('catalog.urls')),
     url(r'^', include('catalog.urls')),
 ]
